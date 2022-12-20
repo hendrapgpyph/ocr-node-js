80c7dbe0deb127913188c53d78d13c1fba16a0f6
Jinom Developer <jinomdev@gmail.com>
2022-12-20 15:06:22 +0800

    update

--- bash/bash.sh
+++ bash/bash.sh
@@ -1,9 +1,11 @@
+apt update
+apt install nano
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 14.15.3
 nvm use 14.15.3
 wget https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz
-tar cmake-3.12.3
+tar cmake-3.12.3.tar.gz
 cd cmake-3.12.3
 ./bootstrap --prefix=/usr/local
 make -j$(nproc)
