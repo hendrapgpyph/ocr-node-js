0bb2a054ab6f4e45bace9e0a482a260346fb4552
Jinom Developer <jinomdev@gmail.com>
2022-12-20 23:06:02 +0800

    update source script

--- bash/bash.sh
+++ bash/bash.sh
@@ -1,7 +1,7 @@
 apt update
 apt install nano
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-source ~/.nvm/nvm.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash \
+. ~/.nvm/nvm.sh
 nvm install 14.15.3
 nvm use 14.15.3
 wget https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz
